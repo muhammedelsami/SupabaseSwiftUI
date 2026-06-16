0c78c8e7d20908f7a26e4f081566aae11d464db9
Muhammed Elşami <muhammed97r@hotmail.com>
2026-06-16 09:11:51 +0300

    Initial Commit

---
+++ SupabaseSwiftUI/SupabaseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupabaseSwiftUIApp.swift
+//  SupabaseSwiftUI
+//
+//  Created by Muhammed Elşami on 16.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct SupabaseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
